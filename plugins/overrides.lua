b456afcb533bb508bc96af1d5aba9d8dc239456b
Leonard Yeung <leonard_yeung@tuta.io>
2023-01-16 18:32:23 +0800

    feat: updated chadNV config for null ls

---
+++ plugins/overrides.lua
@@ -0,0 +1,104 @@
+local M = {}
+
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "html",
+    "css",
+    "javascript",
+    "c",
+    "bash",
+    "cmake",
+    "cpp",
+    "diff",
+    "git_rebase",
+    "gitattributes",
+    "gitcommit",
+    "gitignore",
+    "graphql",
+    "html",
+    "javascript",
+    "jq",
+    "json",
+    "json5",
+    "markdown",
+    "python",
+    "regex",
+    "scala",
+    "solidity",
+    "sql",
+    "svelte",
+    "typescript",
+    "yaml"
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "deno",
+    "angular-language-server",
+    "bash-language-server",
+    "clang-format",
+    "clangd",
+    "cmake-language-server",
+    "cpplint",
+    "cpptools",
+    "cssmodules-language-server",
+    "dockerfile-language-server",
+    "eslint-lsp",
+    "eslint_d",
+    "json-lsp",
+    "json-to-struct",
+    "jsonlint",
+    "luacheck",
+    "luaformatter",
+    "markdownlint",
+    "prettier",
+    "prettierd",
+    "pylint",
+    "python-lsp-server",
+    "rust-analyzer",
+    "rustfmt",
+    "shellcheck",
+    "shellharden",
+    "shfmt",
+    "solang",
+    "solhint",
+    "solidity",
+    "solidity-ls",
+    "sql-formatter",
+    "sqlls",
+    "svelte-language-server",
+    "vim-language-server",
+    "yaml-language-server",
+    "yamlfmt",
+    "yamllint"
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+return M

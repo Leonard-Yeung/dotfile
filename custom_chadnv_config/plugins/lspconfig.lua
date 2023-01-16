9caecbe57794be26363ff1835928a99456cae114
Leonard Yeung <leonardyeung0503@tutanota.com>
2023-01-17 01:02:21 +0800

    merge: with main for chadnv config

---
+++ custom_chadnv_config/plugins/lspconfig.lua
@@ -0,0 +1,13 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+
+local servers = { "angularls", "bashls", "clangd", "cssls", "cssmodules_ls", "dockerls", "eslint", "graphql", "java_language_server", "html", "jqls", "jsonls", "metals", "pylsp", "pyright", "rls", "rust_analyzer", "solang", "solc", "solidity", "solidity_ls", "sqls", "svelte", "syntax_tree", "tsserver", "vimls", "vuels", "yamlls"}
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end

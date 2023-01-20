a79879fdb79549dca75ea5cd52dce1d43a61db97
Leonard Yeung <leonardyeung0503@tutanota.com>
2023-01-21 01:37:49 +0800

    refactor: wrong file structure for chadNV configs

--- custom_chadnv_config/plugins/lspconfig.lua
+++ custom_chadnv_config/plugins/lspconfig.lua
@@ -3,11 +3,34 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
-local servers = { "angularls", "bashls", "clangd", "cssls", "cssmodules_ls", "dockerls", "eslint", "graphql", "java_language_server", "html", "jqls", "jsonls", "metals", "pylsp", "pyright", "rls", "rust_analyzer", "solang", "solc", "solidity", "solidity_ls", "sqls", "svelte", "syntax_tree", "tsserver", "vimls", "vuels", "yamlls"}
+local servers = {
+  "angularls",
+  "bashls",
+  "clangd",
+  "cssls",
+  "cssmodules_ls",
+  "dockerls",
+  "eslint",
+  "graphql",
+  "java_language_server",
+  "html",
+  "jqls",
+  "jsonls",
+  "metals",
+  "pylsp",
+  "pyright",
+  "rls",
+  "rust_analyzer",
+  "solidity_ls",
+  "sqls",
+  "svelte",
+  "syntax_tree",
+  "tsserver",
+  "vimls",
+  "vuels",
+  "yamlls",
+}
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+  lspconfig[lsp].setup { on_attach = on_attach, capabilities = capabilities }
 end

a5169b152db1a8c084783dadaf1a502a29be2b34
Leoanrd Yeung <94115482+Leonard53@users.noreply.github.com>
2023-11-28 02:24:29 +0800

    feat: rewrote nvim config

---
+++ custom_chadnv_config/configs/lspconfig.lua
@@ -0,0 +1,20 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+local coq = require "coq"
+
+-- if you just want default config for the servers then put them in a table
+local servers = { "html", "cssls", "tsserver", "clangd", "pyright" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    coq.lsp_ensure_capabilities {
+      on_attach = on_attach,
+      capabilities = capabilities,
+    },
+  }
+end
+
+--
+-- lspconfig.pyright.setup { blabla}
--- custom_chadnv_config/mappings.lua
+++ custom_chadnv_config/mappings.lua
@@ -1,8 +1,12 @@
+---@type MappingsTable
 local M = {}
 
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+  },
+  v = {
+    [">"] = { ">gv", "indent"},
   },
 }
 

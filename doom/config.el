c9434a7d34221d753a4ad562391197b8ed30d2df
Leoanrd Yeung <94115482+Leonard53@users.noreply.github.com>
2024-01-16 00:26:54 +0800

    feat: added grip mode for markdown preview

--- doom/config.el
+++ doom/config.el
@@ -76,3 +76,6 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(use-package! grip-mode :ensure t
+              :hook ((markdown-mode org-mode) . grip-mode))

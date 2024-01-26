f13541061925fc2830758ef4a061a521cd0bdf60
Leonard Yeung <94115482+Leonard53@users.noreply.github.com>
2024-01-27 02:43:48 +0800

    feat: updated nvim with mini

---
+++ custom_chadnv_config/pluginModules/mini.lua
@@ -0,0 +1,17 @@
+local miniSubmodule = { "surround", "ai", "files", "pick", "basics", "bracketed", "extra", "sessions" }
+return {
+  ---@type NvPluginSpec[]
+  "echasnovski/mini.nvim",
+  version = false,
+  event = "BufEnter",
+  config = function()
+    require("mini.surround").setup()
+    require("mini.ai").setup()
+    require("mini.files").setup()
+    require("mini.pick").setup()
+    require("mini.basics").setup()
+    require("mini.bracketed").setup()
+    require("mini.extra").setup()
+    require("mini.sessions").setup()
+  end,
+}

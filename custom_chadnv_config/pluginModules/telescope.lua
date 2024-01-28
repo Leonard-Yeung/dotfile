1dfb475b94de2282b32934132e25503736ffab96
Leoanrd Yeung <94115482+Leonard53@users.noreply.github.com>
2024-01-28 21:28:14 +0800

    merge: with linux for updated neovim

--- custom_chadnv_config/pluginModules/telescope.lua
+++ custom_chadnv_config/pluginModules/telescope.lua
@@ -2,15 +2,11 @@ return {
   "nvim-telescope/telescope.nvim",
   lazy = false,
   dependencies = { "folke/trouble.nvim", "nvim-lua/plenary.nvim" },
-  config = function()
-    require "plugins.configs.telescope"
-    require("telescope").setup {
-      defaults = {
-        mappings = {
-          i = { ["<c-t>"] = require("trouble.providers.telescope").open_with_trouble },
-          n = { ["<c-t>"] = require("trouble.providers.telescope").open_with_trouble },
-        },
-      },
+  opst = function()
+    local conf = require "plugins.configs.telescope"
+    conf.defaults.mapping = {
+      i = { ["<c-t>"] = require("trouble.providers.telescope").open_with_trouble },
+      n = { ["<c-t>"] = require("trouble.providers.telescope").open_with_trouble },
     }
   end,
 }

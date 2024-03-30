97ffd4d3927652a380f06a27cfd2e46207b5a45d
Leoanrd Yeung <94115482+Leonard53@users.noreply.github.com>
2024-03-30 15:31:09 +0800

    refactor: tailored to linux settings

--- custom_chadnv_config/options.lua
+++ custom_chadnv_config/options.lua
@@ -1,8 +1,8 @@
 require "nvchad.options"
 
 -- add yours here!
-vim.o.guifont = "Hack Nerd Font Mono:h11"
-vim.g.neovide_refresh_rate = 60
+vim.o.guifont = "Hack Nerd Font Mono:h10"
+vim.g.neovide_refresh_rate = 144
 vim.g.neovide_cursor_animation_length = 0.05
 vim.opt.termguicolors = true
 vim.g.neovide_cursor_vfx_mode = "pixiedust"

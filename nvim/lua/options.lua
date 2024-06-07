ee9d7972624228e782fa500b080dda12a3e53195
Leoanrd Yeung <94115482+Leonard53@users.noreply.github.com>
2024-06-08 02:49:02 +0800

    feat: dynamically set refresh rate and font size with environment OS

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,8 +1,6 @@
 require "nvchad.options"
 
 -- add yours here!
-vim.o.guifont = "Hack Nerd Font Mono:h12"
-vim.g.neovide_refresh_rate = 60
 vim.g.neovide_cursor_animation_length = 0.05
 vim.opt.termguicolors = true
 vim.g.neovide_cursor_vfx_mode = "pixiedust"
@@ -10,5 +8,13 @@ vim.g.neovide_remember_window_size = true
 vim.cmd "set linebreak"
 vim.opt.spell = true
 vim.opt.spelllang = { "en_us" }
+
+if vim.fn.has "mac" then
+  vim.o.guifont = "Hack Nerd Font Mono:h12"
+  vim.g.neovide_refresh_rate = 60
+else
+  vim.o.guifont = "Hack Nerd Font Mono:h14"
+  vim.g.neovide_refresh_rate = 144
+end
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!

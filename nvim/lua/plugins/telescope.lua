2252ecd7baa0e3470e85098a69f77da60a5cd8a9
Leoanrd Yeung <94115482+Leonard53@users.noreply.github.com>
2024-06-10 21:19:25 +0800

    wip: telescope custom config

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,5 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  dependencies = { "nvim-lua/plenary.nvim", "BurntSushi/ripgrep", "sharkdp/fd", "nvim-tree/nvim-web-devicons" },
+  event = "VeryLazy",
+}

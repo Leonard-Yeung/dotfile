e58d2b088f3c75a46f45feeba1adefa0cad2593f
Leonard Yeung <leonardyeung@digigear.io>
2024-11-14 19:50:48 +0800

    feat: added user.js config

---
+++ firefoxUserJS/user-overrides.js
@@ -0,0 +1,5 @@
+user_pref("browser.privatebrowsing.autostart", false);
+user_pref("privacy.clearOnShutdown.cookies", false);
+user_pref("privacy.clearOnShutdown.sessions", false);
+user_pref("privacy.clearOnShutdown_v2.cookiesAndStorage", false);
+user_pref("extensions.systemAddon.update.enabled", true);

f72ad56dac2b120bba0215aef4a5bf7f816ff3d1
Leonard Yeung <leonard_yeung@tuta.io>
2025-01-11 14:36:41 +0800

    feat: disabled pocket in firefox userJs

--- firefoxUserJS/user-overrides.js
+++ firefoxUserJS/user-overrides.js
@@ -3,3 +3,4 @@ user_pref("privacy.clearOnShutdown.cookies", false);
 user_pref("privacy.clearOnShutdown.sessions", false);
 user_pref("privacy.clearOnShutdown_v2.cookiesAndStorage", false);
 user_pref("extensions.systemAddon.update.enabled", true);
+user_pref("extensions.pocket.enabled", false);

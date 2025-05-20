96d969788c90c591a215898438b766fcd4fc1d90
Leonard Yeung <leonard_yeung@tuta.io>
2025-05-21 03:09:30 +0800

    chore: remove firefox arnkerfox config

--- firefoxUserJS/user-overrides.js
+++
@@ -1,6 +0,0 @@
-user_pref("browser.privatebrowsing.autostart", false);
-user_pref("privacy.clearOnShutdown.cookies", false);
-user_pref("privacy.clearOnShutdown.sessions", false);
-user_pref("privacy.clearOnShutdown_v2.cookiesAndStorage", false);
-user_pref("extensions.systemAddon.update.enabled", true);
-user_pref("extensions.pocket.enabled", false);
